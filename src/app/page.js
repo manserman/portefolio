ac149fee10daefb4ef042c7b1d60f33fe8f90a1a
manslit <mansourh923@gmail.com>
2024-07-08 17:29:46 +0200

    Commit

--- src/app/page.js
+++ src/app/page.js
@@ -13,24 +13,39 @@ import data_savoir from "./Data/data_savoir";
 import data_exp from "./Data/data_experience";
 import data_projet from "./Data/data_projet";
 
+
 export default function Home() {
+  /********************* DONNEES  **************************/
+
+  /********************* EXPERIENCES  **************************/
   const experiences = data_exp();
+
+  /********************* PROJETS  **************************/
   const projets = data_projet();
+
+  /** Affichage dynamique des projets **/
   const [ind_projet, set_ind_projet] = useState(0);
   const display_project = (e) => {
     set_ind_projet(e.target.id);
   };
 
+  /********************* COMPETENCES  **************************/
   const competences = data_comp();
+
+  /** Affichage dynamique des compétences **/
   const [ind_tab_comp, set_ind_tab_comp] = useState(0);
   const display_comp = (e) => {
     set_ind_tab_comp(e.target.id);
   };
 
+  /********************* FORMATIONS  **************************/
   const formations = data_form();
+
+  /** Affichage dynamique des formations **/
   const [ind_formation, set_ind_formation] = useState(0);
   const formation_carousel = (e) => {
     const sign = e.target.id;
+
     if (sign === "previous") {
       if (ind_formation !== 0) set_ind_formation(ind_formation - 1);
       else set_ind_formation(formations.length - 1);
@@ -41,164 +56,250 @@ export default function Home() {
     }
   };
 
+  /********************* SAVOIRS  **************************/
   const savoirs = data_savoir();
 
-  const s_sideBar = "h-full w-1/5 fixed bg-gray-800 flex flex-col p-5";
-  const s_logo = "mx-auto mt-10 rounded-full overflow-hidden";
-  const s_nav = "text-white text-lg p-3 w-full text-center hover:bg-gray-700 mt-1";
-  const s_nav_section = "flex flex-col items-center justify-start mt-8";
-  const s_main = "flex flex-col w-4/5 ml-auto p-10 space-y-20";
-  const s_hr = "w-full h-0.5 bg-black mt-10";
+  /********************* CONSTANTES DE STYLE  **************************/
+  const s_sideBar = "h-[100%] w-[17%] fixed fixed bg-gray-800 flex flex-col";
+  const s_photo = "mx-auto h-[8em] w-[8em] mt-10 rounded-full flex items-center justify-center font-semibold";
+  const s_logo = "mx-auto mt-20 scale-[115%] rounded-full overflow-hidden";
+  const s_nav = "text-white text-sm p-3 w-full text-center hover:bg-gray-700 mt-1";
+  const s_nav_section = "flex flex-col items-center justify-start mt-4";
+  const s_main = "flex flex-col h-[400%] w-[83%] ml-[17%]";
+  const s_hr = "w-full h-[0.1em] mt-20 bg-black";
 
   return (
-    <div className="flex">
+    <div>
+   
+    <main className="h-full bg-inherit flex flex-row">
+      {/********************* SIDE BAR **********************/}
       <motion.div
         className={s_sideBar}
         initial={{ x: "-100%" }}
         animate={{ x: 0 }}
         transition={{ type: "spring", stiffness: 100 }}
       >
-        <Image className={s_logo} height={100} width={100} src="./img/photo.png" alt="Photo" />
+        <Image className={s_logo} height={300} width={200} src="./img/photo.png" />
+
         <div className={s_nav_section}>
-          <a href="#accueil" className={s_nav}>Accueil</a>
-          <a href="#competences" className={s_nav}>Competences</a>
-          <a href="#experiences" className={s_nav}>Experiences</a>
-          <a href="#savoirs" className={s_nav}>Savoir Etre</a>
-          <a href="#formations" className={s_nav}>Formations</a>
-          <a href="#projets" className={s_nav}>Projets Universitaires</a>
+          <div className={s_nav}>
+            <a href="#accueil">Accueil</a>
+          </div>
+          <div className={s_nav}>
+            <a href="#competences">Competences</a>
+          </div>
+          <div className={s_nav}>
+            <a href="#experiences">Experiences</a>
+          </div>
+          <div className={s_nav}>
+            <a href="#savoirs">Savoir Etre</a>
+          </div>
+          <div className={s_nav}>
+            <a href="#formations">Formations</a>
+          </div>
+          <div className={s_nav}>
+            <a href="#projets">Projets Universitaires</a>
+          </div>
         </div>
       </motion.div>
+
+      {/********************* SECTION PRINCIPALE **********************/}
       <motion.div
         className={s_main}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1 }}
       >
-        <section id="accueil" className="space-y-4">
-          <h1 className="text-4xl font-serif text-black">Mohamadou Mansour HABIBOU HAMANI</h1>
-          <div className="flex items-center mt-4">
-            <Image src="./img/photo.png" alt="Photo" height={100} width={100} className="rounded-full mr-4" />
-            <h2 className="italic font-serif text-black text-2xl">Développeur Java</h2>
+        {/********************* PRESENTATION **********************/}
+
+        <div className="h-[7%] w-[100%] pt-10 pl-10" id="accueil">
+          <span className="text-4xl font-serif text-black">
+            Mohamadou Mansour HABIBOU HAMANI
+          </span>
+          <div className="flex flex-row mt-4">
+            <span className="italic font-serif text-black">
+              Développeur Java
+            </span>
           </div>
-          <p className="text-justify tracking-wide text-base font-serif mt-4">
+          <br></br>
+
+          <div className="text-justify tracking-wide text-sm font-serif w-[85%]">
             Fort d'une expérience de 2 ans en développement Java en alternance, j'ai contribué à la mise à jour du système
             de traçabilité d'Autoliv, passant de Java 6 à Java 11. J'ai assuré le bon fonctionnement des webservices REST
             et des outils d'administration et reporting, en mettant l'accent sur les tests unitaires pour garantir la 
             qualité du code. Mes compétences incluent WildFly, Jersey, Spring Boot, Maven,...
-            Parallèlement, ma formation universitaire m'a doté d'une connaissance approfondie des systèmes d'information web 
-            et des design patterns, avec une expérience pratique en Node.js, Angular, React,.Net et C#.
-          </p>
-          <div className="flex space-x-4 mt-4">
-            <span>&#128233; : mansourh923@gmail.com</span>
-            <span>&#128241; : 06 65 15 40 90</span>
-            <span>&#128664; : Ile de france / Hauts de france</span>
+             Parallèlement, ma formation universitaire m'a doté d'une connaissance approfondie des systèmes d'information web 
+             et des design patterns, avec une expérience pratique en Node.js, Angular, React,.Net et C#.
           </div>
-          <div className="flex justify-center mt-4">
-            <a href="./files/cv.pdf" target="blank" className="hover:underline">Mon CV</a>
+
+          <div className="w-[70%] h-[20%] justify-center mt-4 border-black grid grid-cols-8">
+            <span>&#128233;</span>
+            <span className="-ml-[75%] mt-1.5 text-xs">
+              : mansourh923@gmail.com
+            </span>
+            <span>&#128241;</span>
+            <span className="-ml-[75%] mt-1.5 text-xs">: 06 65 15 40 90</span>
+            <span>&#128664;</span>
+            <span className="-ml-[75%] mt-1.5 text-xs">
+              : Ile de france / Hauts de france
+            </span>
           </div>
-        </section>
+
+          <div className="flex flex-row font-serif justify-center space-x-4 text-sm h-10 w-[20%] absolute top-12 right-10">
+            <a
+              href="./files/cv.pdf"
+              target="blank"
+              className="hover:underline hover:cursor-pointer"
+            >
+              Mon CV
+            </a>
+          </div>
+        </div>
+
         <div className={s_hr}></div>
-        <section id="competences" className="space-y-4">
-          <h2 className="text-3xl font-serif text-black">COMPETENCES</h2>
-          <p className="text-slate-600 text-sm italic">
-            Compétences issues du référentiel présent sur le site du CIGREF (version 2022)
-          </p>
-          <div className="flex space-x-4 mt-4">
-            {competences.map((item, index) => (
-              <motion.div
-                id={index}
-                key={index}
-                onClick={display_comp}
-                className={`p-4 rounded-lg font-semibold text-white ${item.color}`}
-                whileHover={{ scale: 1.1 }}
-              >
-                {item.poste}
-              </motion.div>
-            ))}
+
+        {/********************* COMPETENCES **********************/}
+
+        <div id="competences" className="w-full h-[15%] relative pl-12 pt-10 ">
+          <span className="text-black font-serif text-3xl">COMPETENCES</span>
+          <br />
+          <div className="text-slate-600 text-xs italic mt-2">
+            Compétences issues du référentiel présent sur le site du CIGREF
+            (version 2022)
           </div>
-          <div className="mt-6 space-y-4">
-            {competences[ind_tab_comp].comp.map((item, index) => (
-              <Competence key={index} comp={item} />
-            ))}
+
+          <div className="flex flex-row mx-auto space-x-7 mt-6 h-[17%] w-[70%]">
+            {competences.map((item, index) => {
+              return (
+                <motion.div
+                  id={index}
+                  key={index}
+                  onClick={display_comp}
+                  className={
+                    "h-full w-[100%] flex items-center justify-center border border-black font-semibold text-white " +
+                    item.color
+                  }
+                  whileHover={{ scale: 1.1 }}
+                >
+                  {item.poste}
+                </motion.div>
+              );
+            })}
           </div>
-        </section>
+
+          <div className="no-scrollbar mt-10 overflow-y-auto h-[60%]">
+            {competences[ind_tab_comp].comp.map((item, index) => {
+              return <Competence key={index} comp={item}></Competence>;
+            })}
+          </div>
+        </div>
+
         <div className={s_hr}></div>
-        <section id="experiences" className="space-y-4">
-          <h2 className="text-3xl font-serif text-black">EXPERIENCES</h2>
-          <div className="space-y-4">
-            {experiences.map((item, index) => (
-              <Experience key={index} exp={item} />
-            ))}
+
+        {/********************* EXPERIENCES **********************/}
+        <div
+          id="experiences"
+          className="w-full h-[22%] pt-10 space-y-6 pl-12"
+        >
+          <span className="text-black font-serif text-3xl">EXPERIENCES</span>
+          <div className="flex flex-col h-[90%] space-y-4 overflow-y-auto">
+            {experiences.map((item, index) => {
+              return <Experience key={index} exp={item}></Experience>;
+            })}
           </div>
-        </section>
+        </div>
+
         <div className={s_hr}></div>
-        <section id="savoirs" className="space-y-4">
-          <h2 className="text-3xl font-serif text-black">SAVOIR ETRE</h2>
-          <p className="text-slate-600 text-sm italic">
-            Savoir être issue du référentiel présent sur le site de pôle emploi (ROME 4.0)
-          </p>
-          <table className="mt-4 w-full text-left text-sm">
-            <thead className="bg-gray-800 text-white">
-              <tr>
-                <th className="p-2">Savoir être</th>
-                <th className="p-2">Description</th>
-              </tr>
-            </thead>
-            <tbody>
-              {savoirs.map((item, index) => (
+
+        {/********************* SAVOIR ETRE **********************/}
+
+        <div id="savoirs" className="w-full h-[15%] relative pl-12 pt-10">
+          <span className="text-black font-serif text-3xl">SAVOIR ETRE</span>
+          <br />
+          <div className="text-slate-600 text-xs italic mt-2">
+            Savoir être issue du référentiel présent sur le site de pôle emploi
+            (ROME 4.0)
+          </div>
+
+          <table className="mt-5 text-center h-[80%] w-[85%] overflow-y-auto text-xs">
+            <tr className="bg-gray-800 text-xs text-white font-normal">
+              <th>Savoir être</th>
+              <th>Description</th>
+            </tr>
+            {savoirs.map((item, index) => {
+              return (
                 <tr key={index}>
-                  <td className="p-2">{item.nom}</td>
-                  <td className="p-2">{item.description}</td>
+                  <td>{item.nom}</td>
+                  <td>{item.description}</td>
                 </tr>
-              ))}
-            </tbody>
+              );
+            })}
           </table>
-        </section>
+        </div>
+
         <div className={s_hr}></div>
-        <section id="formations" className="relative space-y-4">
-          <h2 className="text-3xl font-serif text-black">FORMATIONS</h2>
-          <p className="text-slate-600 text-sm italic">
+
+        {/********************* FORMATIONS **********************/}
+        <div
+          id="formations"
+          className="relative w-full h-[14%] pt-10 space-y-6 pl-12"
+        >
+          <span className="text-black font-serif text-3xl">FORMATIONS</span>
+
+          <div className="text-slate-600 text-xs italic mt-2">
             Liste des acquis des formations selon le site France competences
-          </p>
-          <div className="flex items-center justify-between mt-4">
-            <span
-              id="previous"
-              onClick={formation_carousel}
-              className="cursor-pointer text-4xl text-gray-800"
-            >
-              &#10216;
-            </span>
-            <Formation form={formations[ind_formation]} />
-            <span
-              id="next"
-              onClick={formation_carousel}
-              className="cursor-pointer text-4xl text-gray-800"
-            >
-              &#10217;
-            </span>
           </div>
-        </section>
+
+          <span
+            id="previous"
+            onClick={formation_carousel}
+            className="absolute top-[50%] text-6xl text-gray-800 hover:cursor-pointer hover:scale-[130%]"
+          >
+            &#10216;
+          </span>
+
+          <span
+            id="next"
+            onClick={formation_carousel}
+            className="absolute top-[50%] right-[10%] hover:cursor-pointer text-gray-800 text-6xl hover:scale-[130%]"
+          >
+            &#10217;
+          </span>
+
+          <div className="ml-28">
+            <Formation form={formations[ind_formation]}></Formation>
+          </div>
+        </div>
+
         <div className={s_hr}></div>
-        <section id="projets" className="space-y-4">
-          <h2 className="text-3xl font-serif text-black">Projets Universitaires</h2>
-          <div className="flex space-x-4 mt-4">
-            {projets.map((item, index) => (
-              <motion.div
-                key={index}
-                id={index}
-                onClick={display_project}
-                className="p-4 bg-slate-100 border border-black rounded-lg text-center text-black text-sm"
-                whileHover={{ scale: 1.1 }}
-              >
-                {item.titre}
-              </motion.div>
-            ))}
+
+        <div id="projets" className="relative w-full h-[14%] pt-10 pl-12">
+          <span className="text-black font-serif text-3xl">
+            Projets Universitaires
+          </span>
+
+          <div className="flex flex-row items-center justify-center space-x-10 h-[15%] mt-10 w-[100%]">
+            {projets.map((item, index) => {
+              return (
+                <motion.div
+                  key={index}
+                  id={index}
+                  onClick={display_project}
+                  className="bg-slate-100 border-2 border-black w-[20%] flex items-center text-center justify-center h-full hover:scale-110 text-black text-xs"
+                  whileHover={{ scale: 1.1 }}
+                >
+                  {item.titre}
+                </motion.div>
+              );
+            })}
           </div>
-          <div className="mt-6">
-            <Projet projet={projets[ind_projet]} />
+
+          <div className="w-[100%] h-[100%]">
+            <Projet projet={projets[ind_projet]}></Projet>
           </div>
-        </section>
+        </div>
       </motion.div>
+    </main>
     </div>
   );
 }
