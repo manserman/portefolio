78490502c47a875168b82a0718bef9a20fcf0322
manslit <mansourh923@gmail.com>
2024-09-13 17:39:08 +0200

    commmit

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import Experience from "./Components/Experience";
 import Projet from "./Components/Projet";
