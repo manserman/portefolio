04cf3fd4d4e1b8741db907110e6eac408627fbf7
manslit <mansourh923@gmail.com>
2024-10-24 17:04:54 +0200

    Commit_expérience

--- src/app/Data/data_experience.js
+++ src/app/Data/data_experience.js
@@ -4,18 +4,21 @@ export default function data_exp() {
     entreprise: "Autoliv Europe",
     logo: "logo_autoliv.png",
     lieu: "Gournay-en-Braie, France",
-    description: "Mise à jour du système de traçabilité d 'autoliv de java 6 à java 8 puis java 11 et enfin java 17",
+    description:
+      "Mise à jour du système de traçabilité d 'autoliv de java 6 à java 8 puis java 11 et enfin java 17",
     detail:
-      "Mise à jour du système de traçabilité d 'autoliv de java 6 à java 17 suivant une roadmap pré-établie",
+      "Dans le cadre de la modernisation de son système de traçabilité, j’ai rejoint Autoliv, leader mondial des systèmes de sécurité automobile, en tant qu’ingénieur Java back-end. À ce titre, j’ai contribué à l’évolution de solutions critiques pour la sécurité, destinées à améliorer la traçabilité des composants tout au long de la chaîne de production. Mes principales missions incluent :",
     missions: [
+      "Gestion et conception de la mise à jour d'un système de traçabilité de java 6 à java 8 puis java 11 et enfin java 17.",
       "Migration des webservices rest du framework Jersey vers le framework SpringBoot 2.7",
       "Tests D'intégrations ",
       "Réalisation de test Unitaires",
       "CI/CD",
       "Correction de bugs et amélioraiton des performances",
-      "Migration des applets vers des applications java",
+      "Migration des applets vers des applications java avec Icedtea-Web et le protocole JNLP",
       "Vérification et résolution des CVE (failles de sécurité) avec OWASP discovery",
-      "Développement de nouvelles fonctionnalités"
+      "Développement de nouvelles fonctionnalités",
+      "Etude de l'existant",
     ],
     competences: [
       "Conception et développement d'applications",
@@ -23,7 +26,7 @@ export default function data_exp() {
       "Production de la documentation ",
       "Intégration des composants",
       "Identification des besoins",
-      "Innovation"
+      "Innovation",
     ],
     debut: "Septembre 2022",
     fin: "Septembre 2024",
@@ -48,7 +51,8 @@ export default function data_exp() {
       "Conception et développement d'applications",
       "Production de la documentation ",
       "Identification des besoins",
-      "Innovation"],
+      "Innovation",
+    ],
     debut: "Avril 2022",
     fin: "Juin 2022",
     isExperience: true,
